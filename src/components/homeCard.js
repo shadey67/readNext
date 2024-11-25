9346532b492c245dd44600ba71b4d9eaf42b8cb6
shadey67 <owenshade4@gmail.com>
2024-11-25 18:48:35 +0000

    push random

--- src/components/homeCard.js
+++ src/components/homeCard.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { Link } from 'react-router-dom';
 
+
 export default function HomeCard({url, buttonText, icon, text}) {
     return(
         <Link 
